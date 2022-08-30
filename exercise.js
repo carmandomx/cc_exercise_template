7c94b142946e08c0ebfd8f7c1ec41b02ff819dab
Carlos Reyes <carlos.reyes@theksquaregroup.com>
2022-08-30 13:47:41 -0500

    Carlos Reyes

--- exercise.js
+++ exercise.js
@@ -1,8 +1,8 @@
 /** DO NOT CHANGE THE FUNCTION NAME **/
 const cToF = (c) => {
     /* Only make changes below this comment */
-    
-    
+    const f = c * 9/5 + 32
+    return f;
     /* Only make changes below this comment */
 }
 
