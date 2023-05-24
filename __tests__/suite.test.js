8ef2feda18508b871db06ea1f0e8eeebb4f2daf5
Argenis Jose Gonzalez <argenis.gonzalez@theksquaregroup.com>
2023-05-24 11:03:46 -0400

    config done

--- __tests__/suite.test.js
+++ __tests__/suite.test.js
@@ -1,6 +1,26 @@
-const { func } = require('../exercise');
+const countSmileys = require('../exercise');
 
-test('Testing the function exercise', () => { 
-    expect(func()).toBeDefined();
-    
- })
+test('Counting valid smiley faces - Test 1', () => {
+  const arr = [':)', ';(', ';}', ':-D'];
+  expect(countSmileys(arr)).toBe(2);
+});
+
+test('Counting valid smiley faces - Test 2', () => {
+  const arr = [';D', ':-(', ':-)', ';~)'];
+  expect(countSmileys(arr)).toBe(3);
+});
+
+test('Counting valid smiley faces - Test 3', () => {
+  const arr = [';]', ':[', ';*', ':$', ';-D'];
+  expect(countSmileys(arr)).toBe(1);
+});
+
+test('Counting valid smiley faces - Empty array', () => {
+  const arr = [];
+  expect(countSmileys(arr)).toBe(0);
+});
+
+test('Counting valid smiley faces - All happy faces', () => {
+  const arr = [':)', ';)', ':D'];
+  expect(countSmileys(arr)).toBe(3);
+});
